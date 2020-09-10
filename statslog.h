82c85f5d2bb33c51e35e0d3ac26c00474ef9207d
Suren Baghdasaryan <surenb@google.com>
2020-09-09 20:19:02 -0700

    lmkd: report kill reason, and meminfo details to statsd for each kill
    Information like free memory and swap as well as kill reason would be
    useful for understanding regressions in the number of lmk kills in the
    field. Pass this information to statsd after each kill.
    
    Bug: 168117803
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Ic46aed3c85b880b32ac5ad61b55f90e0d33517c7

--- statslog.h
+++ statslog.h
@@ -37,6 +37,16 @@ struct memory_stat {
     int64_t process_start_time_ns;
 };
 
+struct kill_stat {
+    int32_t uid;
+    char *taskname;
+    int32_t kill_reason;
+    int32_t oom_score;
+    int32_t min_oom_score;
+    int64_t free_mem_kb;
+    int64_t free_swap_kb;
+};
+
 #ifdef LMKD_LOG_STATS
 
 #define MEMCG_PROCESS_MEMORY_STAT_PATH "/dev/memcg/apps/uid_%u/pid_%d/memory.stat"
@@ -56,17 +66,13 @@ stats_write_lmk_state_changed(int32_t state);
  * Logs the event when LMKD kills a process to reduce memory pressure.
  * Code: LMK_KILL_OCCURRED = 51
  */
-int
-stats_write_lmk_kill_occurred(int32_t uid, char const* process_name,
-                              int32_t oom_score, int32_t min_oom_score,
-                              struct memory_stat *mem_st);
+int stats_write_lmk_kill_occurred(struct kill_stat *kill_st, struct memory_stat *mem_st);
 
 /**
  * Logs the event when LMKD kills a process to reduce memory pressure.
  * Code: LMK_KILL_OCCURRED = 51
  */
-int stats_write_lmk_kill_occurred_pid(int32_t uid, int pid, int32_t oom_score,
-                                      int32_t min_oom_score,
+int stats_write_lmk_kill_occurred_pid(int pid, struct kill_stat *kill_st,
                                       struct memory_stat* mem_st);
 
 /**
