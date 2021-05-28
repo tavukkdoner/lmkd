c8c7e4fbd9834cfde5166a7b04ddee25e627d338
Suren Baghdasaryan <surenb@google.com>
2021-05-27 18:15:44 -0700

    lmkd: Include total GPU memory usage in killinfo reports
    /sys/fs/bpf/map_gpu_mem_gpu_mem_total_map BPF map exposes total GPU
    allocations size. Include this value into killinfo reports to track GPU
    allocation size at the time of the kill.
    
    Bug: 189366037
    Test: lmkd_unit_test
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Icc1ed8ab2593530fa293ff9c82f6c8dc400485f5

--- lmkd.cpp
+++ lmkd.cpp
@@ -53,6 +53,9 @@
 
 #include "statslog.h"
 
+#define BPF_FD_JUST_USE_INT
+#include "BpfSyscallWrappers.h"
+
 /*
  * Define LMKD_TRACE_KILLS to record lmkd kills in kernel traces
  * to profile and correlate with OOM kills
@@ -432,6 +435,7 @@ union meminfo {
         /* fields below are calculated rather than read from the file */
         int64_t nr_file_pages;
         int64_t dmabuf_heap_pool;
+        int64_t total_gpu_kb;
     } field;
     int64_t arr[MI_FIELD_COUNT];
 };
@@ -1800,6 +1804,18 @@ static int64_t read_dmabuf_total_pools_kb() {
     return parse_int64(buf, &res) ? res : -1;
 }
 
+static int64_t read_gpu_total_kb() {
+    static int fd = android::bpf::bpfFdGet("/sys/fs/bpf/map_gpu_mem_gpu_mem_total_map", BPF_F_RDONLY);
+    static constexpr uint64_t kBpfKeyGpuTotalUsage = 0;
+    uint64_t value;
+
+    if (fd < 0) {
+        return 0;
+    }
+
+    return android::bpf::findMapEntry(fd, &kBpfKeyGpuTotalUsage, &value) ? 0 : (int64_t)(value / 1024);
+}
+
 static int meminfo_parse(union meminfo *mi) {
     static struct reread_data file_data = {
         .filename = MEMINFO_PATH,
@@ -1825,6 +1841,7 @@ static int meminfo_parse(union meminfo *mi) {
     mi->field.nr_file_pages = mi->field.cached + mi->field.swap_cached +
         mi->field.buffers;
     mi->field.dmabuf_heap_pool = read_dmabuf_total_pools_kb();
+    mi->field.total_gpu_kb = read_gpu_total_kb();
 
     return 0;
 }
@@ -1943,6 +1960,7 @@ static void killinfo_log(struct proc* procp, int min_oom_score, int rss_kb,
     android_log_write_int32(ctx, wi->wakeups_since_event);
     android_log_write_int32(ctx, wi->skipped_wakeups);
     android_log_write_int32(ctx, (int32_t)min(swap_kb, INT32_MAX));
+    android_log_write_int32(ctx, (int32_t)mi->field.total_gpu_kb);
 
     android_log_write_list(ctx, LOG_ID_EVENTS);
     android_log_reset(ctx);
