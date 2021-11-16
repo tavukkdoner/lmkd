05f8b64d83c496cd76b839cdd5bd20ff0f43ea3d
Suren Baghdasaryan <surenb@google.com>
2021-11-16 11:50:29 -0800

    lmkd: Implement watchdog thread
    To detect lmkd being stuck on a syscall for prolonged period of time,
    introduce a watchdog thread which gets set when lmkd starts handling of
    events and is reset after handling is done. If it takes more than the
    timeout period (2 sec) to handle an event, watchdog wakes up and kills
    the least important process to prevent mounting memory pressure caused
    by lmkd lockup. After a kill, watchdog will wait for the reset for
    another timeout period and kill again. This repeats until lmkd unlocks
    and resets the watchdog.
    
    Bug: 201671997
    Test: induce random sleep in lmkd main handler and observe watchdog kills
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I56a55834582e11c06cc6cf9da3bc7380e634b301

---
+++ watchdog.cpp
@@ -0,0 +1,85 @@
+/*
+ *  Copyright 2021 Google, Inc
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#define LOG_TAG "lowmemorykiller"
+
+#include <log/log.h>
+
+#include "watchdog.h"
+
+static void* watchdog_main(void* param) {
+    Watchdog *watchdog = (Watchdog*)param;
+    int reset_cnt;
+
+    for (;;) {
+        // Wait for watchdog to be set
+        reset_cnt = watchdog->wait_for_set();
+
+        while (true) {
+            // Wait for watchdog to either be reset or to timeout
+            if (watchdog->wait_for_reset(reset_cnt)) {
+                // Watchdog was reset
+                break;
+            }
+            // Call watchdog callback and keep waiting for the reset for another timeout period
+            watchdog->bite();
+        }
+    }
+
+    return NULL;
+}
+
+bool Watchdog::start() {
+    pthread_t thread;
+
+    if (pthread_create(&thread, NULL, watchdog_main, this)) {
+        ALOGE("pthread_create failed: %s", strerror(errno));
+        return false;
+    }
+    return true;
+}
+
+void Watchdog::set() {
+    std::scoped_lock<std::mutex> lock(mutex_);
+    set_cnt_++;
+    cond_.notify_one();
+}
+
+void Watchdog::reset() {
+    std::scoped_lock<std::mutex> lock(mutex_);
+    reset_cnt_++;
+    cond_.notify_one();
+}
+
+int Watchdog::wait_for_set() {
+    std::unique_lock<std::mutex> lock(mutex_);
+    while (set_cnt_ == reset_cnt_) {
+        cond_.wait(lock);
+    }
+    return reset_cnt_;
+}
+
+bool Watchdog::wait_for_reset(int reset_cnt) {
+    std::chrono::duration timout = std::chrono::seconds(timeout_);
+    std::unique_lock<std::mutex> lock(mutex_);
+    while (reset_cnt_ == reset_cnt) {
+        if (cond_.wait_for(lock, timout) == std::cv_status::timeout) {
+            // Wait timed out
+            return false;
+        }
+    }
+    return true;
+}
