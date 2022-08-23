24d98f8492ee189da3c242f55c7428ccf7d5fa0a
Suren Baghdasaryan <surenb@google.com>
2022-08-23 13:25:56 -0700

    lmkd: Remove process record after it is killed by lmkd watchdog
    After lmkd watchdog kills a process, its record should be removed.
    
    Bug: 243567425
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I70bb2a432df8088ebc9865fbc36b065738248d19

--- lmkd.cpp
+++ lmkd.cpp
@@ -2157,6 +2157,7 @@ static void watchdog_callback() {
         if (reaper.kill({ target.pidfd, target.pid, target.uid }, true) == 0) {
             ALOGW("lmkd watchdog killed process %d, oom_score_adj %d", target.pid, oom_score);
             killinfo_log(&target, 0, 0, 0, NULL, NULL, NULL, NULL, NULL);
+            pid_remove(target.pid);
             break;
         }
         prev_pid = target.pid;
