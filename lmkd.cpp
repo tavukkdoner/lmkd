ae7806a271e7f37101b5cf6fc1d0bd64d7e8e1d9
Carlos Galo <carlosgalo@google.com>
2024-02-12 13:56:15 -0800

    lmkd: Introduce kill strategy based on direct reclaim length
    Add kill reason for when the device is stuck in direct reclaim for
    longer than the configurable threshold. Only allow configurable
    threshold, and direct reclaim stuck detection, if we are running on bpf
    ring buffer supported kernel.
    
    Test: Verified direct reclaim stuck kill log with memory pressure test
    Test: m
    Bug: 244232958
    Change-Id: I1156899874d2eb7e0f4b61597741087c110b3414
    Signed-off-by: Carlos Galo <carlosgalo@google.com>

--- lmkd.cpp
+++ lmkd.cpp
@@ -230,6 +230,7 @@ static int64_t stall_limit_critical;
 static bool use_psi_monitors = false;
 static int kpoll_fd;
 static bool delay_monitors_until_boot;
+static int direct_reclaim_threshold_ms;
 static struct psi_threshold psi_thresholds[VMPRESS_LEVEL_COUNT] = {
     { PSI_SOME, 70 },    /* 70ms out of 1sec for partial stall */
     { PSI_SOME, 100 },   /* 100ms out of 1sec for partial stall */
@@ -2632,6 +2633,7 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
     long since_thrashing_reset_ms;
     int64_t workingset_refault_file;
     bool critical_stall = false;
+    long direct_reclaim_duration_ms;
 
     if (clock_gettime(CLOCK_MONOTONIC_COARSE, &curr_tm) != 0) {
         ALOGE("Failed to get current time");
@@ -2687,6 +2689,7 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
     /* Identify reclaim state */
     if (isBpfRingBufferSupported &&
         (direct_reclaim_start_tm.tv_sec != 0 || direct_reclaim_start_tm.tv_nsec != 0)) {
+        direct_reclaim_duration_ms = get_time_diff_ms(&direct_reclaim_start_tm, &curr_tm);
         reclaim = DIRECT_RECLAIM;
     } else if (!isBpfRingBufferSupported && (vs.field.pgscan_direct != init_pgscan_direct)) {
         init_pgscan_direct = vs.field.pgscan_direct;
@@ -2849,6 +2852,11 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
             min_score_adj = PERCEPTIBLE_APP_ADJ + 1;
         }
         check_filecache = true;
+    } else if (reclaim == DIRECT_RECLAIM && direct_reclaim_threshold_ms > 0 &&
+               direct_reclaim_duration_ms > direct_reclaim_threshold_ms) {
+        kill_reason = DIRECT_RECL_STUCK;
+        snprintf(kill_desc, sizeof(kill_desc), "device is stuck in direct reclaim (%" PRId64 "%%)",
+                 direct_reclaim_duration_ms);
     } else if (check_filecache) {
         int64_t file_lru_kb = (vs.field.nr_inactive_file + vs.field.nr_active_file) * page_k;
 
@@ -3918,6 +3926,12 @@ static bool update_props() {
     filecache_min_kb = GET_LMK_PROPERTY(int64, "filecache_min_kb", 0);
     stall_limit_critical = GET_LMK_PROPERTY(int64, "stall_limit_critical", 100);
     delay_monitors_until_boot = GET_LMK_PROPERTY(bool, "delay_monitors_until_boot", false);
+    direct_reclaim_threshold_ms = GET_LMK_PROPERTY(int64, "direct_reclaim_threshold_ms", 0);
+
+    if (!isBpfRingBufferSupported && direct_reclaim_threshold_ms > 0) {
+        ALOGW("direct_reclaim_threshold_ms not supported on kernel < 5.8");
+        direct_reclaim_threshold_ms = 0;
+    }
 
     reaper.enable_debug(debug_process_killing);
 
