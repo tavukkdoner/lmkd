1148c6b9c134dda928fbf6b5bff2f0dd7cb9c5d7
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2024-09-09 09:06:11 +0300

    LMKD: Fix taskname_dict memory allocation and free elements of taskname_dict

--- lmkd.cpp
+++ lmkd.cpp
@@ -2607,7 +2607,7 @@ static bool is_not_excluded(char *do_not_kill_tasknames, char *taskname) {
     strncpy(tasknames, do_not_kill_tasknames, len);
 
     /* Allocate memory for the array */
-    char **taskname_dict = malloc(num_tasknames * sizeof(char *));
+    char **taskname_dict = new char *[num_tasknames];
 
     /* Split the tasknames and store */
     int i = 0;
@@ -2626,6 +2626,9 @@ static bool is_not_excluded(char *do_not_kill_tasknames, char *taskname) {
         }
     }
 
+    for (int i = 0; i < num_tasknames; i++) {
+        free(taskname_dict[i]);
+    }
     free(tasknames);
     free(taskname_dict);
 
